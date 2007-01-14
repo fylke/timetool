2a1785eb65c3f46e3588637aeeb0a55f999531bc
magnus.falk <magnus.falk@d527ebd3-c326-0410-8431-79df68f5f61f>
2007-01-14 19:02:22 +0000

    Renamed to a better name since there should be possible to have several users in the system, each with their own project set.

---
+++ TimeTool/src/persistency/projects/ProjectSet.java
@@ -0,0 +1,90 @@
+package persistency.projects;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ProjectSet {
+  private int id;
+  private Map<Integer, Company> companies;
+  
+  public ProjectSet() {
+    companies = new TreeMap<Integer, Company>();
+  }
+
+  public Company getCompany(final int id) {
+    return companies.get(id);
+  }
+  
+  public Collection<Company> getCompanies() {
+    return companies.values();
+  }
+  
+  /**
+   * @return the ID of the project set
+   */
+  public int getId() {
+    return id;
+  }
+
+  /**
+   * @param id the ID to set
+   */
+  public void setId(final int id) {
+    this.id = id;
+  }
+
+  public void addCompany(final Company company) {
+    companies.put(company.getId(), company);
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    StringBuilder objRep = new StringBuilder();
+    objRep.append("id: " + id + "\n");
+        
+    for (Company company : companies.values()) {
+      objRep.append("Companies:\n");
+      objRep.append(company.toString());
+    }
+    
+    return objRep.toString();
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    final int PRIME = 31;
+    int result = 1;
+    result = PRIME * result + ((companies == null) ? 0 : companies.hashCode());
+    result = PRIME * result + id;
+    return result;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    final ProjectSet other = (ProjectSet) obj;
+    if (companies == null) {
+      if (other.companies != null)
+        return false;
+    } else if (!companies.equals(other.companies))
+      return false;
+    if (id != other.id)
+      return false;
+    return true;
+  }
+}
---
+++ TimeTool/src/persistency/projects/ProjectSetParser.java
@@ -0,0 +1,53 @@
+package persistency.projects;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class ProjectSetParser extends DefaultHandler implements ContentHandler {
+  private XMLReader reader;
+  private ProjectSet projectSet;
+  
+  public ProjectSetParser(final XMLReader reader) {
+    this.reader = reader;
+  }
+  
+  public void setTargetObject(final ProjectSet projectSet) {
+    this.projectSet = projectSet;
+  }
+
+  public ProjectSet parse(final InputSource is) throws SAXException, IOException, 
+      ParserConfigurationException {
+    reader.setContentHandler(this);
+    reader.parse(is);
+    
+    return projectSet;
+  }
+
+  @Override
+  public void startElement(final String uri, final String localName, 
+                           final String qName, final Attributes attributes) 
+      throws SAXException {
+    if ("projectSet".equals(qName)) {
+      projectSet.setId(Integer.parseInt(attributes.getValue("id")));
+    }
+    else if ("company".equals(qName)) {
+      final Company company = new Company();
+      company.setId(Integer.parseInt(attributes.getValue("id")));
+      assert(projectSet != null);
+
+      projectSet.addCompany(company);
+           
+      final ContentHandler companyHandler = 
+        new CompanyHandler(attributes, reader, this, company);
+      reader.setContentHandler(companyHandler);
+    }
+  }
+}
