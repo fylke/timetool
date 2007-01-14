fefd41556eb53d5e40dfc786a171dc9e414a6148
magnus.falk <magnus.falk@d527ebd3-c326-0410-8431-79df68f5f61f>
2007-01-14 19:00:38 +0000

    Forgot to add equals and hashCode, disturbing consequences

--- TimeTool/src/persistency/projects/Company.java
+++ TimeTool/src/persistency/projects/Company.java
@@ -1,14 +1,19 @@
 package persistency.projects;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Company {
   private int id;
   private String name;
   private String shortName;
   private String employeeId;
-  private List<Project> projects;
+  private Map<Integer, Project> projects;
+  
+  public Company() {
+    projects = new TreeMap<Integer, Project>();
+  }
   
   public int getId() {
     return id;
@@ -26,11 +31,8 @@ public class Company {
     return employeeId;
   }
 
-  public List<Project> getProjects() {
-    if (projects == null) {
-      projects = new ArrayList<Project>();
-    }
-    return projects;
+  public Collection<Project> getProjects() {
+    return projects.values();
   }
 
   public void setId(final int id) {
@@ -50,9 +52,77 @@ public class Company {
   }
   
   public void addProject(final Project project) {
-    if (projects == null) {
-      projects = new ArrayList<Project>();
+    projects.put(project.getId(), project);
+  }
+  
+  /* (non-Javadoc)
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    StringBuilder objRep = new StringBuilder();
+    objRep.append("id: " + id + "\n");
+    objRep.append("name: " + name + "\n");
+    objRep.append("shortName: " + shortName + "\n");
+    objRep.append("employeeId: " + employeeId + "\n");
+        
+    for (Project project : projects.values()) {
+      objRep.append("Projects:\n");
+      objRep.append(project.toString());
     }
-    projects.add(project);
+    
+    return objRep.toString().trim();
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    final int PRIME = 31;
+    int result = 1;
+    result = PRIME * result + ((employeeId == null) ? 0 : employeeId.hashCode());
+    result = PRIME * result + id;
+    result = PRIME * result + ((name == null) ? 0 : name.hashCode());
+    result = PRIME * result + ((projects == null) ? 0 : projects.hashCode());
+    result = PRIME * result + ((shortName == null) ? 0 : shortName.hashCode());
+    return result;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    final Company other = (Company) obj;
+    if (employeeId == null) {
+      if (other.employeeId != null)
+        return false;
+    } else if (!employeeId.equals(other.employeeId))
+      return false;
+    if (id != other.id)
+      return false;
+    if (name == null) {
+      if (other.name != null)
+        return false;
+    } else if (!name.equals(other.name))
+      return false;
+    if (projects == null) {
+      if (other.projects != null)
+        return false;
+    } else if (!projects.equals(other.projects))
+      return false;
+    if (shortName == null) {
+      if (other.shortName != null)
+        return false;
+    } else if (!shortName.equals(other.shortName))
+      return false;
+    return true;
   }
 }
--- TimeTool/src/persistency/projects/Project.java
+++ TimeTool/src/persistency/projects/Project.java
@@ -1,17 +1,22 @@
 package persistency.projects;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.Map;
+import java.util.TreeMap;
 
-import persistency.year.Activity;
 
 public class Project {
   private int id;
   private String name;
   private String shortName;
   private String code;
-  private List<Activity> activities;
-  private List<Project> subProjects;
+  private Map<Integer, Activity> activities;
+  private Map<Integer, Project> subProjects;
+  
+  public Project() {
+    activities = new TreeMap<Integer, Activity>();
+    subProjects = new TreeMap<Integer, Project>();
+  }
   
   public int getId() {
     return id;
@@ -31,18 +36,18 @@ public class Project {
 
   /**
    * 
-   * @return a list of the activities in this project, null if there are none
+   * @return the activities in this project
    */
-  public List<Activity> getActivities() {
-    return activities;
+  public Collection<Activity> getActivities() {
+    return activities.values();
   }
   
   /**
    * 
-   * @return a list of the subprojects to this project, null if there are none
+   * @return the subprojects to this project
    */
-  public List<Project> getSubProjects() {
-    return subProjects;
+  public Collection<Project> getSubProjects() {
+    return subProjects.values();
   }
   
   public void setId(final int id) {
@@ -62,16 +67,92 @@ public class Project {
   }
 
   public void addActivity(final Activity activity) {
-    if (activities == null) {
-      activities = new ArrayList<Activity>();
-    }
-    activities.add(activity);
+    activities.put(activity.getId(), activity);
   }
   
   public void addSubProject(final Project subProject) {
-    if (subProjects == null) {
-      subProjects = new ArrayList<Project>();
+    subProjects.put(subProject.getId(), subProject);
+  }
+  
+  /* (non-Javadoc)
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    StringBuilder objRep = new StringBuilder();
+    objRep.append("id: " + id + "\n");
+    objRep.append("name: " + name + "\n");
+    objRep.append("shortName: " + shortName + "\n");
+    objRep.append("code: " + code + "\n");
+        
+    for (Project subProject : subProjects.values()) {
+      objRep.append("Subprojects:\n");
+      objRep.append(subProject.toString());
     }
-    subProjects.add(subProject);
+
+    for (Activity activity : activities.values()) {
+      objRep.append("Activities:\n");
+      objRep.append(activity.toString());
+    }
+    
+    return objRep.toString().trim();
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    final int PRIME = 31;
+    int result = 1;
+    result = PRIME * result + ((activities == null) ? 0 : activities.hashCode());
+    result = PRIME * result + ((code == null) ? 0 : code.hashCode());
+    result = PRIME * result + id;
+    result = PRIME * result + ((name == null) ? 0 : name.hashCode());
+    result = PRIME * result + ((shortName == null) ? 0 : shortName.hashCode());
+    result = PRIME * result + ((subProjects == null) ? 0 : subProjects.hashCode());
+    return result;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    final Project other = (Project) obj;
+    if (activities == null) {
+      if (other.activities != null)
+        return false;
+    } else if (!activities.equals(other.activities))
+      return false;
+    if (code == null) {
+      if (other.code != null)
+        return false;
+    } else if (!code.equals(other.code))
+      return false;
+    if (id != other.id)
+      return false;
+    if (name == null) {
+      if (other.name != null)
+        return false;
+    } else if (!name.equals(other.name))
+      return false;
+    if (shortName == null) {
+      if (other.shortName != null)
+        return false;
+    } else if (!shortName.equals(other.shortName))
+      return false;
+    if (subProjects == null) {
+      if (other.subProjects != null)
+        return false;
+    } else if (!subProjects.equals(other.subProjects))
+      return false;
+    return true;
   }
 }
