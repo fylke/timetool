77d29923ee44e55255b644a3b093125a59de9a14
magnus.falk <magnus.falk@d527ebd3-c326-0410-8431-79df68f5f61f>
2007-01-14 21:30:17 +0000

    Updated to handle activities as well

--- trunk/TimeTool/test/persistency/projects/TestProjectsFactory.java
+++ trunk/TimeTool/test/persistency/projects/TestProjectsFactory.java
@@ -9,7 +9,10 @@ public class TestProjectsFactory {
   public static transient final String projName = "testProject";
   public static transient final String projShortName = "testProj";
   public static transient final String projCode = "testCode";
-
+  public static transient final String actName = "activityName";
+  public static transient final String actShortName = "actName";
+  
+  
   private static TestProjectsFactory factoryInstance;
   private transient XmlUtils xmlUtils;
   
@@ -44,24 +47,38 @@ public class TestProjectsFactory {
     company.setEmployeeId(empId + id);
     
     for (int i = 1; i <= projSetConfig.nrOfProjsPerComp; i++) {
-      company.addProject(getProject(i + (id * 10), projSetConfig.projDepth));
+      company.addProject(getProject(i + (id * 10), projSetConfig.projDepth,
+                                    projSetConfig));
     }
     
     return company;
   }
   
-  public Project getProject(final int id, final int nestingDepth) {
+  public Project getProject(final int id, final int depth, 
+                            final ProjSetConfig projSetConfig) {
     Project child = new Project();
     child.setId(id);
     child.setName(projName + id);
     child.setShortName(projShortName + id);
     child.setCode(projCode + id);
 
-    if (nestingDepth > 0) {
-      child.addSubProject(getProject(id * 10, nestingDepth - 1));
+    for (int i = 0; i < projSetConfig.nrOfActsPerProj; i++) {
+      child.addActivity(getActivity(i + (id *10)));
+    }
+
+    if (depth > 0) {
+      child.addSubProject(getProject(id * 10, depth - 1, projSetConfig));
     }
     
     return child;
+  }
+  
+  public Activity getActivity(final int id) {
+    Activity act = new Activity(id);
+    act.setName(actName + id);
+    act.setShortName(actShortName + id);
+    
+    return act;
   }
   
   public String getXmlProjSetWithConfig(final ProjSetConfig projSetConfig) {
@@ -85,7 +102,7 @@ public class TestProjectsFactory {
     sb.append(indent + "<employeeId>" + empId + id + "</employeeId>\n");
 
     for (int i = 1; i <= projSetConfig.nrOfProjsPerComp; i++) {
-      getXmlProject(i + (id * 10), projSetConfig.projDepth, sb);
+      getXmlProject(i + (id * 10), projSetConfig.projDepth, projSetConfig, sb);
     }
     indent = xmlUtils.decIndent(indent);
     sb.append(indent + "</company>\n");
@@ -93,8 +110,9 @@ public class TestProjectsFactory {
     return sb.toString();
   }
 
-  public String  getXmlProject(final int id, final int depth, 
-                               final StringBuilder sb) {
+  public String getXmlProject(final int id, final int depth, 
+                              final ProjSetConfig projSetConfig, 
+                              final StringBuilder sb) {
     String indent = xmlUtils.indent(Integer.toString(id).length());
 
     sb.append(indent + "<project id=\"" + id + "\">\n");
@@ -103,12 +121,29 @@ public class TestProjectsFactory {
     sb.append(indent + "<shortName>" + projShortName + id + "</shortName>\n");
     sb.append(indent + "<code>" + projCode + id + "</code>\n");
 
+    for (int i = 0; i < projSetConfig.nrOfActsPerProj; i++) {
+      getXmlActivity(i + (id * 10), sb);
+    }
+    
     if (depth > 0) {
-      getXmlProject(id * 10, depth - 1, sb);
+      getXmlProject(id * 10, depth - 1, projSetConfig, sb);
     }
 
     indent = xmlUtils.decIndent(indent);
     sb.append(indent + "</project>\n");
+    
+    return sb.toString();
+  }
+  
+  public String getXmlActivity(final int id, final StringBuilder sb) {
+    String indent = xmlUtils.indent(Integer.toString(id).length());
+    
+    sb.append(indent + "<activity id=\"" + id + "\">\n");
+    indent = xmlUtils.incIndent(indent);
+    sb.append(indent + "<name>" + actName + id + "</name>\n");
+    sb.append(indent + "<shortName>" + actShortName + id + "</shortName>\n");
+    indent = xmlUtils.decIndent(indent);
+    sb.append(indent + "</activity>\n");
     
     return sb.toString();
   }
