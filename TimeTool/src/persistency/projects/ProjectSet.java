75dca6e6786421fb88af06d2268e7da2c62a3a06
magnus.falk <magnus.falk@d527ebd3-c326-0410-8431-79df68f5f61f>
2007-03-25 22:52:39 +0000

    Fiddled with equality and checking of args

--- TimeTool/src/persistency/projects/ProjectSet.java
+++ TimeTool/src/persistency/projects/ProjectSet.java
@@ -4,8 +4,9 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.TreeMap;
 
+import persistency.ItemAlreadyDefinedException;
+
 public class ProjectSet {
-  private int id;
   private Map<Integer, Company> companies;
   
   public ProjectSet() {
@@ -19,45 +20,23 @@ public class ProjectSet {
   public Collection<Company> getCompanies() {
     return companies.values();
   }
-  
-  /**
-   * @return the ID of the project set
-   */
-  public int getId() {
-    return id;
-  }
 
-  /**
-   * @param id the ID to set
-   */
-  public void setId(final int id) {
-    this.id = id;
-  }
-
-  public void addCompany(final Company company) {
-    companies.put(company.getId(), company);
-  }
-  
-  /**
-   * Finds an unused ID for a company in this set.
-   * TODO Improve this, maybe use a hash of Company name?
-   * @return
-   */
-  public int allocateCompanyId() {
-    int suggestion = companies.keySet().size();
-    while(companies.keySet().contains(suggestion)) {
-      suggestion++;
+  public void addCompany(final Company company) 
+      throws ItemAlreadyDefinedException {
+    if (companies.containsKey(company.hashCode())) {
+      throw new ItemAlreadyDefinedException("Company " + company.getName() + 
+                                            " is already defined!");
     }
-    return suggestion;
+    companies.put(company.hashCode(), company);
   }
-
+  
   /* (non-Javadoc)
    * @see java.lang.Object#toString()
    */
   @Override
   public String toString() {
     StringBuilder objRep = new StringBuilder();
-    objRep.append("id: " + id + "\n");
+    objRep.append("id: " + hashCode() + "\n");
         
     for (Company company : companies.values()) {
       objRep.append("Companies:\n");
@@ -75,7 +54,6 @@ public class ProjectSet {
     final int PRIME = 31;
     int result = 1;
     result = PRIME * result + ((companies == null) ? 0 : companies.hashCode());
-    result = PRIME * result + id;
     return result;
   }
 
@@ -95,8 +73,6 @@ public class ProjectSet {
       if (other.companies != null)
         return false;
     } else if (!companies.equals(other.companies))
-      return false;
-    if (id != other.id)
       return false;
     return true;
   }
