d1fe81019e6970e40712a77b09289c08a66a70ae
magnus.falk <magnus.falk@d527ebd3-c326-0410-8431-79df68f5f61f>
2007-10-31 21:25:05 +0000

    Got lost in transit

---
+++ trunk/TimeTool/src/persistency/settings/Settings.java
@@ -0,0 +1,56 @@
+/*
+ * Settings.java
+ *
+ * Created on October 31, 2007, 9:23 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package persistency.settings;
+
+import persistency.projects.Company;
+import persistency.projects.ProjectSet;
+
+
+public interface Settings {
+    Company getEmployedAt();
+
+    int getEmployerId();
+
+    String getFirstName();
+
+    String getLastName();
+
+    int getLunchBreak();
+
+    ProjectSet getProjectSet();
+
+    int getProjectSetId();
+
+    UserSettings.OvertimeType getTreatOvertimeAs();
+
+    void setEmployedAt(final String employedAt);
+
+    void setEmployedAt(final int employedAt);
+
+    void setFirstName(final String userFirstName);
+
+    void setInstance(final Settings settings);
+
+    void setLastName(final String userLastName);
+
+    void setLunchBreak(final String lunchBreak);
+
+    void setLunchBreak(final int lunchBreak);
+
+    void setProjectSet(ProjectSet projectSet);
+
+    void setProjectSetId(final String projectSetId);
+
+    void setProjectSetId(final int projectSetId);
+
+    void setTreatOvertimeAs(UserSettings.OvertimeType treatOvertimeAs);
+
+    void setTreatOvertimeAs(String treatOvertimeAs);   
+}
