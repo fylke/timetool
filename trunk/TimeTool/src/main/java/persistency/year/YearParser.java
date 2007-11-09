4af2e946cee43862c50728cedb527295ec85fa87
magnus.falk <magnus.falk@d527ebd3-c326-0410-8431-79df68f5f61f>
2007-11-09 23:29:40 +0000

    Cleaning up

--- trunk/TimeTool/src/main/java/persistency/year/YearParser.java
+++ trunk/TimeTool/src/main/java/persistency/year/YearParser.java
@@ -12,8 +12,7 @@ import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
 
 
-
-public class YearParser extends DefaultHandler implements ContentHandler {
+public class YearParser extends DefaultHandler {
   private XMLReader reader;
   private Year year;
   private SearchControl wanted;
@@ -40,19 +39,16 @@ public class YearParser extends DefaultHandler implements ContentHandler {
 
   @Override
   public void startElement(final String uri, final String localName, 
-                           final String qName, final Attributes attributes) 
+                           final String qName, final Attributes attrs) 
       throws SAXException {
     if ("year".equals(qName)) {
-      year.setId(Integer.parseInt(attributes.getValue("id")));
-    }
-    else if ("month".equals(qName)) {
+      year.setId(Integer.parseInt(attrs.getValue("id")));
+    } else if ("month".equals(qName)) {
       assert(year != null);
-      final Month month = new Month(Short.parseShort(attributes.getValue("id")), 
-                                    year.getId());
+      final Month month = new Month(Integer.parseInt(attrs.getValue("id")), year.getId());
       year.addMonth(month);
            
-      final ContentHandler monthHandler = 
-        new MonthHandler(attributes, reader, this, month, wanted);
+      final ContentHandler monthHandler = new MonthHandler(attrs, reader, this, month, wanted);
       reader.setContentHandler(monthHandler);
     }
   }
