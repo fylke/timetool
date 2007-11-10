4a012744953a710b2dd36a1f9921f5e7260a5aae
magnus.falk <magnus.falk@d527ebd3-c326-0410-8431-79df68f5f61f>
2007-11-10 21:29:49 +0000

    Changed from string to File

--- trunk/TimeTool/src/test/java/persistency/PersistencyHandlerStorDirTest.java
+++ trunk/TimeTool/src/test/java/persistency/PersistencyHandlerStorDirTest.java
@@ -2,29 +2,13 @@ package persistency;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.util.Set;
-import java.util.TreeSet;
 import java.util.prefs.Preferences;
 
-import org.joda.time.DateTime;
-import org.joda.time.ReadableDateTime;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import persistency.projects.ProjSetConfig;
-import persistency.projects.ProjectSet;
-import persistency.projects.ProjectsFactory;
-import persistency.settings.Settings;
-import persistency.settings.SettingsFactory;
-import persistency.year.SearchControl;
-import persistency.year.Year;
-import persistency.year.YearConfig;
-import persistency.year.YearFactory;
 
 public final class PersistencyHandlerStorDirTest {
   private static PersistencyHandler ph;
@@ -48,18 +32,18 @@ public final class PersistencyHandlerStorDirTest {
   
   @Test
   public final void testGetDefaultStorageDir() throws Exception {
-  	final String expected = System.getProperty("user.home") + File.separator + ".timetool";
-  	final String result = ph.getStorageDir();
+  	final File expected = new File(System.getProperty("user.home") + File.separator + ".timetool");
+  	final File result = ph.getStorageDir();
   	
     assertEquals("The default dir gotten back was not the expected one!", expected, result);
   }
   
   @Test
   public final void testSetAndGetStorageDir() throws Exception {
-    final String testDir = System.getProperty("user.home") + File.separator + "test";
+    final File testDir = new File(System.getProperty("user.home") + File.separator + "test");
     ph.setStorageDir(testDir);
     
-    final String result = ph.getStorageDir();
+    final File result = ph.getStorageDir();
     assertEquals("The dir set and dir gotten back were not equal!", testDir, result);
   }
 }
