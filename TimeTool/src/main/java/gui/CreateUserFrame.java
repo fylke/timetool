c3b940ff899edfcab45030ee61fb4515763d5d3b
magnus.falk <magnus.falk@d527ebd3-c326-0410-8431-79df68f5f61f>
2007-12-09 00:24:11 +0000

    New way of invoking reading and writing Settings

--- TimeTool/src/main/java/gui/CreateUserFrame.java
+++ TimeTool/src/main/java/gui/CreateUserFrame.java
@@ -15,48 +15,48 @@ import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 import javax.swing.border.EtchedBorder;
 
+import persistency.PersistencyException;
 import persistency.projects.Company;
 import persistency.projects.ProjectSet;
-import persistency.projects.ProjectSetReader;
 import persistency.settings.Settings;
-import persistency.settings.UserAdder;
-import persistency.settings.UserSettings;
 
 import com.atticlabs.zonelayout.swing.ZoneLayout;
 import com.atticlabs.zonelayout.swing.ZoneLayoutFactory;
 
 public class CreateUserFrame extends JFrame implements ActionListener {
   private static final long serialVersionUID = 1L;
-  
+
   private ZoneLayout basePanelLayout;
   private JPanel basePanel;
   private ZoneLayout upperPanelLayout;
   private JPanel upperPanel;
   private ZoneLayout lowerPanelLayout;
   private JPanel lowerPanel;
-  
+
   private JLabel firstNameLabel;
   private JTextField firstNameTF;
-  
+
   private JLabel lastNameLabel;
   private JTextField lastNameTF;
-  
+
   private JLabel compLabel;
   private MyComboBox compCoB;
   private String compEmptyMsg = "Inga företag definierade";
   private JButton newCompBT;
- 
+
   private JButton cancelBT;
   private JButton applyBT;
   private JButton okBT;
-  
-  public CreateUserFrame() {
+
+  private Settings userSettings;
+
+  public CreateUserFrame(Settings userSettings) {
     super();
-    new ProjectSetReader().read();
+    this.userSettings = userSettings;
     initComponents();
     pack();
   }
-  
+
   public void actionPerformed(final ActionEvent e) {
     if (e.getSource().equals(newCompBT)) {
       java.awt.EventQueue.invokeLater(
@@ -70,58 +70,63 @@ public class CreateUserFrame extends JFrame implements ActionListener {
           }
         );
     } else if (e.getSource().equals(okBT)) {
-      if (validInput()) {
-        Settings user = UserSettings.getInstance();
-        user.setFirstName(firstNameTF.getText());
-        user.setLastName(lastNameTF.getText());
-        user.setEmployedAt(((Company) compCoB.getSelected()).getId());
-        java.awt.EventQueue.invokeLater(new UserAdder(this));
+      if (isValidInput()) {
+      	storeUser(userSettings);
         setVisible(false);
         dispose();
-      }      
+      } else {
+
+      }
     } else if (e.getSource().equals(cancelBT)) {
       setVisible(false);
       dispose();
     } else if (e.getSource().equals(applyBT)) {
-      if (validInput()) {
-        Settings user = UserSettings.getInstance();
-        user.setFirstName(firstNameTF.getText());
-        user.setLastName(lastNameTF.getText());
-        user.setEmployedAt(((Company) compCoB.getSelected()).getId());
-        firstNameTF.setText("");
-        lastNameTF.setText("");
-        java.awt.EventQueue.invokeLater(new UserAdder(this));
+      if (isValidInput()) {
+      	storeUser(userSettings);
       }
     }
   }
-  
+
+  void storeUser(final Settings user) {
+    user.setFirstName(firstNameTF.getText());
+    user.setLastName(lastNameTF.getText());
+    user.setEmployedAt(((Company) compCoB.getSelected()).getId());
+
+    try {
+			user.store();
+		} catch (PersistencyException e) {
+			JOptionPane.showMessageDialog(this, e.getMessage(),
+                                    "File error", JOptionPane.ERROR_MESSAGE);
+		}
+  }
+
   private void initComponents() {
     setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
     setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 
     setTitle("Ny användare");
     setLocationRelativeTo(null); // Centers the window on the screen.
-    
+
     basePanelLayout = ZoneLayoutFactory.newZoneLayout();
     basePanelLayout.addRow("a~-a");
     basePanelLayout.addRow("b.>b");
-    
+
     basePanel = new JPanel(basePanelLayout);
-      
+
     upperPanelLayout = ZoneLayoutFactory.newZoneLayout();
     upperPanelLayout.addRow("a>a.b-~b", "valueRow");
     upperPanelLayout.addRow("....6...", "valueRow");
     upperPanelLayout.addRow("c>c2d2e.", "comboRow");
     upperPanelLayout.addRow("....6...", "comboRow");
-    
+
     upperPanel = new JPanel(upperPanelLayout);
     upperPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED), "Skapa ny användare"), BorderFactory.createEmptyBorder(5, 5, 5, 5)));
-            
+
     lowerPanelLayout = ZoneLayoutFactory.newZoneLayout();
     lowerPanelLayout.addRow("a>a2c2o");
-    
+
     lowerPanel = new JPanel(lowerPanelLayout);
-       
+
     upperPanelLayout.insertTemplate("valueRow");
     firstNameLabel = new JLabel("Förnamn:");
     upperPanel.add(firstNameLabel, "a");
@@ -133,7 +138,7 @@ public class CreateUserFrame extends JFrame implements ActionListener {
     upperPanel.add(lastNameLabel, "a");
     lastNameTF = new JTextField();
     upperPanel.add(lastNameTF, "b");
-      
+
     upperPanelLayout.insertTemplate("comboRow");
     compLabel = new JLabel("Jobbar:");
     upperPanel.add(compLabel, "c");
@@ -144,7 +149,7 @@ public class CreateUserFrame extends JFrame implements ActionListener {
     newCompBT.addActionListener(this);
     upperPanel.add(newCompBT, "e");
     basePanel.add(upperPanel, "a");
-    
+
     lowerPanel = new JPanel();
     applyBT = new JButton("Spara");
     applyBT.addActionListener(this);
@@ -156,35 +161,35 @@ public class CreateUserFrame extends JFrame implements ActionListener {
     okBT.addActionListener(this);
     lowerPanel.add(okBT, "o");
     basePanel.add(lowerPanel, "b");
-    
+
     add(basePanel);
   }
-  
+
   private Vector<MyComboBoxDisplayable> getComboContents() {
     Vector<MyComboBoxDisplayable> disps = new Vector<MyComboBoxDisplayable>();
-    ProjectSet ps = UserSettings.getInstance().getProjectSet();
+    ProjectSet ps = userSettings.getProjectSet();
     if (ps.getCompanies() != null) {
       disps.addAll(ps.getCompanies());
     }
     return disps;
   }
-  
-  private boolean validInput() {
+
+  private boolean isValidInput() {
     StringBuilder errorMsg = new StringBuilder();
     if (firstNameTF.getText().isEmpty()) {
        errorMsg.append("Förnamn\n");
     }
-    
+
     if (lastNameTF.getText().isEmpty()) {
        errorMsg.append("Efternamn\n");
     }
-    
+
     if (compCoB.getSelected() == null) {
       errorMsg.append("Arbetsgivare\n");
     }
-       
+
     errorMsg.trimToSize();
-    
+
     if (errorMsg.length() > 0) {
       JOptionPane.showMessageDialog(this,
                                     "Följande information saknas " +
