d8df4ba8ccae0662eeef0f8db638ae312d2f4d64
magnus.falk <magnus.falk@d527ebd3-c326-0410-8431-79df68f5f61f>
2007-12-09 00:41:11 +0000

    Passing Settings as a constructor arg

--- TimeTool/src/main/java/gui/CreateActFrame.java
+++ TimeTool/src/main/java/gui/CreateActFrame.java
@@ -21,54 +21,54 @@ import persistency.projects.ActAdder;
 import persistency.projects.Company;
 import persistency.projects.Project;
 import persistency.projects.ProjectSet;
-import persistency.settings.UserSettings;
+import persistency.settings.Settings;
 
 import com.atticlabs.zonelayout.swing.ZoneLayout;
 import com.atticlabs.zonelayout.swing.ZoneLayoutFactory;
 
-public class CreateActFrame extends JFrame implements 
+public class CreateActFrame extends JFrame implements
     ActionListener, FocusListener {
   private static final long serialVersionUID = 1L;
-    
-  ProjectSet projectSet;
-  
+
+  private final ProjectSet projectSet;
+
   private ZoneLayout basePanelLayout;
   private JPanel basePanel;
   private ZoneLayout upperPanelLayout;
   private JPanel upperPanel;
   private ZoneLayout lowerPanelLayout;
   private JPanel lowerPanel;
-  
+
   private JLabel nameLabel;
   private JTextField nameTF;
-  
+
   private JLabel shortNameLabel;
   private JTextField shortNameTF;
-  
+
   private JLabel reportCodeLabel;
   private JTextField reportCodeTF;
-  
+
   private JLabel compLabel;
   private MyComboBox compCoB;
   private String compEmptyMsg = "Inga företag definierade";
   private JButton newCompBT;
-  
+
   private JLabel projLabel;
   private MyComboBox projCoB;
   private String projEmptyMsg = "Inga projekt definierade";
   private JButton newProjBT;
-  
+
   private JButton cancelBT;
   private JButton applyBT;
   private JButton okBT;
-  
-  public CreateActFrame(final ProjectSet projectSet) {
+
+  public CreateActFrame(final Settings user) {
     super();
-    this.projectSet = projectSet;
+    this.projectSet = user.getProjectSet();
     initComponents();
     pack();
   }
-  
+
   /**
    * Update the combo boxes in case companies or projects have been added.
    */
@@ -76,9 +76,9 @@ public class CreateActFrame extends JFrame implements
     compCoB.setContents(getComboContents("company"));
     projCoB.setContents(getComboContents("projects"));
   }
-  
+
   public void focusLost(FocusEvent e) {}
-  
+
   public void actionPerformed(final ActionEvent e) {
     if (e.getSource().equals(newCompBT)) {
       java.awt.EventQueue.invokeLater(
@@ -95,7 +95,7 @@ public class CreateActFrame extends JFrame implements
       java.awt.EventQueue.invokeLater(
           new Runnable() {
             public void run() {
-              final JFrame createProj = new CreateProjectFrame(UserSettings.getInstance().getProjectSet());
+              final JFrame createProj = new CreateProjectFrame(projectSet);
               createProj.setVisible(true);
             }
           }
@@ -104,16 +104,16 @@ public class CreateActFrame extends JFrame implements
       if (validInput()) {
         final Company comp = (Company) compCoB.getSelected();
         final Project proj = (Project) projCoB.getSelected();
-        java.awt.EventQueue.invokeLater(new ActAdder(this, 
-                                                     comp.getId(), 
+        java.awt.EventQueue.invokeLater(new ActAdder(this,
+                                                     comp.getId(),
                                                      proj.getId(),
-                                                     nameTF.getText(), 
+                                                     nameTF.getText(),
                                                      shortNameTF.getText(),
                                                      reportCodeTF.getText(),
-                                                     UserSettings.getInstance().getProjectSet()));
+                                                     projectSet));
       setVisible(false);
       dispose();
-      }      
+      }
     } else if (e.getSource().equals(cancelBT)) {
       setVisible(false);
       dispose();
@@ -124,44 +124,44 @@ public class CreateActFrame extends JFrame implements
         reportCodeTF.setText("");
         final Company comp = (Company) compCoB.getSelected();
         final Project proj = (Project) projCoB.getSelected();
-        java.awt.EventQueue.invokeLater(new ActAdder(this, 
-                                                     comp.getId(), 
+        java.awt.EventQueue.invokeLater(new ActAdder(this,
+                                                     comp.getId(),
                                                      proj.getId(),
-                                                     nameTF.getText(), 
+                                                     nameTF.getText(),
                                                      shortNameTF.getText(),
                                                      reportCodeTF.getText(),
                                                      projectSet));
       }
     }
   }
-  
+
   private void initComponents() {
     setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
     setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 
     setTitle("Aktiviteter");
     setLocationRelativeTo(null); // Centers the window on the screen.
-    
+
     basePanelLayout = ZoneLayoutFactory.newZoneLayout();
     basePanelLayout.addRow("a~-a");
     basePanelLayout.addRow("b.>b");
-    
+
     basePanel = new JPanel(basePanelLayout);
-      
+
     upperPanelLayout = ZoneLayoutFactory.newZoneLayout();
     upperPanelLayout.addRow("a>a.b-~b", "valueRow");
     upperPanelLayout.addRow("....6...", "valueRow");
     upperPanelLayout.addRow("c>c2d2e.", "comboRow");
     upperPanelLayout.addRow("....6...", "comboRow");
-    
+
     upperPanel = new JPanel(upperPanelLayout);
     upperPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED), "Skapa ny aktivitet"), BorderFactory.createEmptyBorder(5, 5, 5, 5)));
-            
+
     lowerPanelLayout = ZoneLayoutFactory.newZoneLayout();
     lowerPanelLayout.addRow("a>a2c2o");
-    
+
     lowerPanel = new JPanel(lowerPanelLayout);
-       
+
     upperPanelLayout.insertTemplate("valueRow");
     nameLabel = new JLabel("Namn:");
     upperPanel.add(nameLabel, "a");
@@ -173,13 +173,13 @@ public class CreateActFrame extends JFrame implements
     upperPanel.add(shortNameLabel, "a");
     shortNameTF = new JTextField();
     upperPanel.add(shortNameTF, "b");
-    
+
     upperPanelLayout.insertTemplate("valueRow");
     reportCodeLabel = new JLabel("Rapportkod:");
     upperPanel.add(reportCodeLabel, "a");
     reportCodeTF = new JTextField();
     upperPanel.add(reportCodeTF, "b");
-    
+
     upperPanelLayout.insertTemplate("comboRow");
     compLabel = new JLabel("Företag:");
     upperPanel.add(compLabel, "c");
@@ -189,7 +189,7 @@ public class CreateActFrame extends JFrame implements
     newCompBT = new JButton("Nytt");
     newCompBT.addActionListener(this);
     upperPanel.add(newCompBT, "e");
-    
+
     upperPanelLayout.insertTemplate("comboRow");
     projLabel = new JLabel("Projekt:");
     upperPanel.add(projLabel, "c");
@@ -200,7 +200,7 @@ public class CreateActFrame extends JFrame implements
     newProjBT.addActionListener(this);
     upperPanel.add(newProjBT, "e");
     basePanel.add(upperPanel, "a");
-    
+
     lowerPanel = new JPanel();
     applyBT = new JButton("Spara");
     applyBT.addActionListener(this);
@@ -212,44 +212,44 @@ public class CreateActFrame extends JFrame implements
     okBT.addActionListener(this);
     lowerPanel.add(okBT, "o");
     basePanel.add(lowerPanel, "b");
-    
+
     add(basePanel);
   }
-  
+
   private Vector<MyComboBoxDisplayable> getComboContents(final String kind) {
     Vector<MyComboBoxDisplayable> disps = new Vector<MyComboBoxDisplayable>();
     if ("company".equalsIgnoreCase(kind) && projectSet.getCompanies() != null) {
       disps.addAll(projectSet.getCompanies());
     } else if ("project".equalsIgnoreCase(kind)) {
       final Company currComp = (Company) compCoB.getSelected();
-      
+
       if (currComp != null && currComp.getProjects() != null) {
         disps.addAll(currComp.getProjects());
       }
     }
     return disps;
   }
-  
+
   private boolean validInput() {
     StringBuilder errorMsg = new StringBuilder();
     if (nameTF.getText().isEmpty()) {
        errorMsg.append("Aktivitetens namn\n");
     }
-    
+
     if (reportCodeTF.getText().isEmpty()) {
        errorMsg.append("Aktivitetens rapportkod\n");
     }
-    
+
     if (compCoB.getSelected() == null) {
       errorMsg.append("Företag med vilket aktiviteten skall associeras\n");
     }
-    
+
     if (projCoB.getSelected() == null) {
       errorMsg.append("Projekt med vilket aktiviteten skall associeras\n");
     }
-    
+
     errorMsg.trimToSize();
-    
+
     if (errorMsg.length() > 0) {
       JOptionPane.showMessageDialog(this,
                                     "F�ljande information saknas " +
