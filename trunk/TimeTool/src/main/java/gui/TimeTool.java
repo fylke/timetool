b2047e53cb8d98e2eb241ac2d600434b32aefd83
magnus.falk <magnus.falk@d527ebd3-c326-0410-8431-79df68f5f61f>
2007-12-09 00:08:31 +0000

    New way of initializing

--- trunk/TimeTool/src/main/java/gui/TimeTool.java
+++ trunk/TimeTool/src/main/java/gui/TimeTool.java
@@ -1,51 +1,57 @@
 package gui;
 
 import java.awt.ComponentOrientation;
+import java.io.FileNotFoundException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
-import persistency.settings.SettingsReader;
+import persistency.PersistencyException;
+import persistency.settings.Settings;
+import persistency.settings.UserSettings;
 
 public class TimeTool extends JFrame {
   private static final long serialVersionUID = 1L;
-  
+
   private JTabbedPane background;
   private JPanel dayViewPane;
   private JPanel weekViewPane;
+  private Settings user;
 
-  public TimeTool() {
+  public TimeTool(final Settings userSettings) {
     super();
+    this.user = userSettings;
     initComponents();
     pack();
   }
 
-  public static void main(final String[] args) {
-    java.awt.EventQueue.invokeLater(
-      new Runnable() {
-        public void run() {
-          if (new SettingsReader().read()) {
-            new TimeTool().setVisible(true);
-          }
-        }
-      }
-    );
+  public static void main(final String... args) {
+  	Settings userSettings = new UserSettings();
+    try {
+			userSettings.populate();
+		} catch (FileNotFoundException e) {
+			new CreateUserFrame(userSettings).setVisible(true);
+		} catch (PersistencyException e) {
+			throw new RuntimeException("Exception in initialization: " +  e.getMessage(), e);
+		}
+
+    new TimeTool(userSettings).setVisible(true);
   }
-  
+
   private void initComponents() {
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 
     setTitle("TimeTool");
     setLocationRelativeTo(null); // Centers the window on the screen.
-    
+
     background = new JTabbedPane();
-    dayViewPane = new DayViewPanel();
-    weekViewPane = new WeekViewPanel();
-    
+    dayViewPane = new DayViewPanel(user);
+    weekViewPane = new WeekViewPanel(user);
+
     add(background);
     background.add(dayViewPane);
     background.add(weekViewPane);
-  }    
+  }
 }
