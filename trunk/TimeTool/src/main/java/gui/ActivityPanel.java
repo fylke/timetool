538fbf9525516436a1433095075a1249a27732f7
magnus.falk <magnus.falk@d527ebd3-c326-0410-8431-79df68f5f61f>
2007-12-09 00:40:54 +0000

    Passing Settings as a constructor arg

--- trunk/TimeTool/src/main/java/gui/ActivityPanel.java
+++ trunk/TimeTool/src/main/java/gui/ActivityPanel.java
@@ -16,48 +16,51 @@ import javax.swing.JPanel;
 
 import persistency.projects.Company;
 import persistency.projects.Project;
+import persistency.settings.Settings;
 import persistency.year.WorkDay;
 
 import com.atticlabs.zonelayout.swing.ZoneLayout;
 import com.atticlabs.zonelayout.swing.ZoneLayoutFactory;
 
-public class ActivityPanel extends JPanel implements ActionListener, 
+public class ActivityPanel extends JPanel implements ActionListener,
     FocusListener {
   private static final long serialVersionUID = 1L;
-  
+
+  private final Settings user;
   private final WorkDay currentDay;
   private int actId;
-  
+
   private Font font;
   private ZoneLayout layout;
   private ZoneLayout layoutUpper;
   private ZoneLayout layoutLowerLeft;
   private ZoneLayout layoutLowerRight;
-  
+
   private JPanel upperPanel;
   private JLabel activityLabel;
   private MyComboBox actCoB;
   private String actCoBEmptyMsg = "Inga aktiviteter definerade";
   private JButton createNewActBT;
-    
+
   private TimePanel timePanel;
-  
+
   private JPanel lowerLeftPanel;
   private JCheckBox includeLunchCB;
   private JLabel includeLunchLabel;
-  
+
   private JPanel lowerRightPanel;
   private JButton deleteTabBT;
-  private JButton saveTabBT; 
+  private JButton saveTabBT;
 
-  public ActivityPanel(final WorkDay currentDay) {
+  public ActivityPanel(final WorkDay currentDay, Settings user) {
     super();
     this.currentDay = currentDay;
+    this.user = user;
     initComponents();
   }
 
-  public ActivityPanel(final WorkDay currentDay, final int actId) {
-    this(currentDay);
+  public ActivityPanel(final WorkDay currentDay, Settings user, final int actId) {
+    this(currentDay, user);
     this.actId = actId;
   }
 
@@ -78,20 +81,20 @@ public class ActivityPanel extends JPanel implements ActionListener,
       java.awt.EventQueue.invokeLater(
           new Runnable() {
             public void run() {
-              final JFrame createActFrame = 
-                new CreateActFrame(currentDay.getUserSettings().getProjectSet());
+              final JFrame createActFrame =
+                new CreateActFrame(user);
               createActFrame.setVisible(true);
             }
           }
         );
     }
   }
-  
+
   private void initComponents() {
     font = new Font("Arial", 1, 14);
-    
+
     setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-    
+
     layout = ZoneLayoutFactory.newZoneLayout();
     layout.addRow("a<..~.a");
     layout.addRow("...6...");
@@ -99,16 +102,16 @@ public class ActivityPanel extends JPanel implements ActionListener,
     layout.addRow("...6...");
     layout.addRow("l<l3b>b");
     setLayout(layout);
-    
+
     layoutUpper = ZoneLayoutFactory.newZoneLayout();
     layoutUpper.addRow("a2c2n");
-    
+
     layoutLowerLeft = ZoneLayoutFactory.newZoneLayout();
     layoutLowerLeft.addRow("c2l");
-   
+
     layoutLowerRight = ZoneLayoutFactory.newZoneLayout();
     layoutLowerRight.addRow("d2s");
-    
+
     upperPanel = new JPanel(layoutUpper);
     activityLabel = new JLabel("Aktivitet:");
     activityLabel.setFont(font);
@@ -116,30 +119,30 @@ public class ActivityPanel extends JPanel implements ActionListener,
     actCoB = new MyComboBox(getComboContents(), actCoBEmptyMsg);
     actCoB.setSelectedIndex(0);
     upperPanel.add(actCoB, "c");
-    
+
     createNewActBT = new JButton("Ny");
     createNewActBT.setToolTipText("Skapa ny aktivitetstyp");
     createNewActBT.addActionListener(this);
     upperPanel.add(createNewActBT, "n");
-    
+
     add(upperPanel, "a");
-    
+
     timePanel = new TimePanel();
-    
+
     add(timePanel, "t");
-    
+
     if (currentDay.getActivity(actId) != null) {
       lowerLeftPanel = new JPanel(layoutLowerLeft);
       includeLunchCB = new JCheckBox();
       lowerLeftPanel.add(includeLunchCB, "c");
-      includeLunchLabel = 
+      includeLunchLabel =
         new JLabel("Inkludera lunch (" +
                    currentDay.getActivity(actId).getLunchLenght() + "min)");
       lowerLeftPanel.add(includeLunchLabel, "l");
-      
+
       add(lowerLeftPanel, "l");
     }
-    
+
     lowerRightPanel = new JPanel(layoutLowerRight);
     deleteTabBT = new JButton("Ta bort");
     deleteTabBT.setToolTipText("Ta bort aktivitet");
@@ -147,18 +150,18 @@ public class ActivityPanel extends JPanel implements ActionListener,
     saveTabBT = new JButton("Spara");
     saveTabBT.setToolTipText("Spara aktivitet");
     lowerRightPanel.add(saveTabBT, "s");
-    
+
     add(lowerRightPanel, "b");
-    
+
     if (actCoB.getSelected() != null) {
       setName(actCoB.getSelected().getShortDispString());
     } else {
       setName(actCoBEmptyMsg);
     }
   }
-  
+
   private Vector<MyComboBoxDisplayable> getComboContents() {
-    final Vector<MyComboBoxDisplayable> acts = 
+    final Vector<MyComboBoxDisplayable> acts =
       new Vector<MyComboBoxDisplayable>();
     for (Company comp : currentDay.getUserSettings().getProjectSet().getCompanies()) {
       for(Project proj : comp.getProjects()) {
