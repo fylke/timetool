5e45e0400ad71b346d4389a46fdb86eb71bae2df
magnus.falk <magnus.falk@d527ebd3-c326-0410-8431-79df68f5f61f>
2007-12-09 00:31:29 +0000

    Passing Settings as a constructor arg

--- TimeTool/src/main/java/gui/WeekViewPanel.java
+++ TimeTool/src/main/java/gui/WeekViewPanel.java
@@ -8,39 +8,44 @@ import javax.swing.JPanel;
 
 import org.joda.time.LocalDateTime;
 
+import persistency.settings.Settings;
+
 import com.atticlabs.zonelayout.swing.ZoneLayout;
 import com.atticlabs.zonelayout.swing.ZoneLayoutFactory;
 
 public class WeekViewPanel extends JPanel {
   private static final long serialVersionUID = 1L;
-  
+
   private ZoneLayout layout;
   private LocalDateTime date;
   private JLabel thisWeek;
   private TriangleButton previous;
   private TriangleButton next;
-   
-  public WeekViewPanel() {
+
+  private Settings user;
+
+  public WeekViewPanel(final Settings user) {
     super();
+    this.user = user;
     initComponents();
   }
 
   private void initComponents() {
     setName("Veckovy");
-    
+
     layout = ZoneLayoutFactory.newZoneLayout();
     layout.addRow("p>pt^-...tn<n");
     layout.addRow("......6......");
     layout.addRow("w<-~........w");
-    
+
     setLayout(layout);
     setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-    
+
     date = new LocalDateTime();
     thisWeek = new JLabel("Vecka " + date.getWeekOfWeekyear());
     thisWeek.setFont(new Font("Arial", 1, 20));
     add(thisWeek, "t");
-    
+
     previous = new TriangleButton(TriangleButton.LEFT);
     add(previous, "p");
     next = new TriangleButton(TriangleButton.RIGHT);
