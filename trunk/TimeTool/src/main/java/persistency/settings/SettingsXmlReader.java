fcd0d23044ff63575492943240ea6f8edf6d2c92
magnus.falk <magnus.falk@d527ebd3-c326-0410-8431-79df68f5f61f>
2007-12-16 23:27:23 +0000

    Removed the factory

--- trunk/TimeTool/src/main/java/persistency/settings/SettingsXmlReader.java
+++ trunk/TimeTool/src/main/java/persistency/settings/SettingsXmlReader.java
@@ -12,47 +12,39 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 
-import persistency.FileParserFactory;
 import persistency.PersistencyException;
+import persistency.XmlReader;
 
-public class SettingsXmlReader implements SettingsFileReader {
+public class SettingsXmlReader extends XmlReader implements SettingsFileReader {
 	/* (non-Javadoc)
 	 * @see persistency.settings.SettingsReader#populate(persistency.settings.Settings)
 	 */
-  public void populate(final Settings settings, final File absPath)
-  		throws FileNotFoundException, PersistencyException {
-//    PersistencyUtils ph = new PersistencyUtils();
+	public void populate(final Settings settings, final File absPath)
+			throws FileNotFoundException, PersistencyException {
+		final XMLReader xmlReader = initXmlReader();
 
-//    File storDir = ph.getStorageDir();
-//    storDir.mkdir();
-//    File storageFile = new File(storDir, absFilename);
+		final SettingsParser sp = new SettingsParser(xmlReader);
+		sp.setTargetObject(settings);
 
-    final Reader fr = new FileReader(absPath);
-    XMLReader settingsParser;
+		final Reader fr = new FileReader(absPath);
 		try {
-			settingsParser = FileParserFactory.getParser("settings");
-
-			// Sending in the object we want to populate.
-      final SettingsParser sp = ((SettingsParser) settingsParser.getContentHandler());
-      sp.setTargetObject(settings);
-
-			settingsParser.parse(new InputSource(fr));
-    } catch (final SAXException e) {
-      throw new PersistencyException("There was a parser problem when " +
-                                     "reading the settings file. " +
-                                     e.getMessage(), e);
-    } catch (final ParserConfigurationException e) {
-      throw new PersistencyException("There was a problem initializing " +
-                                     "a parser while reading the settings " +
-                                     "file. " + e.getMessage(), e);
-    } catch (final IOException e) {
-      throw new PersistencyException("There was an IO problem problem " +
-                                     "while reading the settings " +
-                                     "file. " + e.getMessage(), e);
+			sp.parse(new InputSource(fr));
+		} catch (final SAXException e) {
+			throw new PersistencyException("There was a parser problem when " +
+																		 "reading the settings file. " +
+																		 e.getMessage(), e);
+		} catch (final ParserConfigurationException e) {
+			throw new PersistencyException("There was a problem initializing " +
+																		 "a parser while reading the settings " +
+																		 "file. " + e.getMessage(), e);
+		} catch (final IOException e) {
+			throw new PersistencyException("There was an IO problem problem " +
+																		 "while reading the settings " +
+																		 "file. " + e.getMessage(), e);
 		} finally {
-      try {
-        fr.close();
-      } catch (final IOException e) { /* We don't care if closing fails... */ }
-    }
-  }
+			try {
+				fr.close();
+			} catch (final IOException e) { /* We don't care if closing fails... */ }
+		}
+	}
 }
