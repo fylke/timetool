0e12a41fb19fb6532cbc093f339e0d48aebaae3b
magnus.falk <magnus.falk@d527ebd3-c326-0410-8431-79df68f5f61f>
2007-12-16 01:28:58 +0000

    Got rid of SearchControl

--- trunk/TimeTool/src/test/java/persistency/year/YearConfig.java
+++ trunk/TimeTool/src/test/java/persistency/year/YearConfig.java
@@ -2,21 +2,18 @@ package persistency.year;
 
 
 public class YearConfig {
-  final int year;
-  final int nrOfMonths;
-  final int nrOfDaysEachMonth;
-  final int nrOfActsEachDay;
-  final SearchControl searchControl;
+	final int year;
+	final int nrOfMonths;
+	final int nrOfDaysEachMonth;
+	final int nrOfActsEachDay;
 
-  public YearConfig(final int year, final int nrOfMonths,
-                    final int nrOfDaysEachMonth,
-                    final int nrOfActsEachDay,
-                    final SearchControl searchControl) {
-    super();
-    this.year = year;
-    this.nrOfMonths = nrOfMonths;
-    this.nrOfDaysEachMonth = nrOfDaysEachMonth;
-    this.nrOfActsEachDay = nrOfActsEachDay;
-    this.searchControl = searchControl;
-  }
+	public YearConfig(final int year, final int nrOfMonths,
+										final int nrOfDaysEachMonth,
+										final int nrOfActsEachDay) {
+		super();
+		this.year = year;
+		this.nrOfMonths = nrOfMonths;
+		this.nrOfDaysEachMonth = nrOfDaysEachMonth;
+		this.nrOfActsEachDay = nrOfActsEachDay;
+	}
 }
