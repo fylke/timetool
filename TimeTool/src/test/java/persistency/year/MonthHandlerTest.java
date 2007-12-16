ec696d2dfa9269442dd0fb7d97118e252d49faf7
magnus.falk <magnus.falk@d527ebd3-c326-0410-8431-79df68f5f61f>
2007-12-16 01:26:26 +0000

    Got rid of SearchControl

--- TimeTool/src/test/java/persistency/year/MonthHandlerTest.java
+++ TimeTool/src/test/java/persistency/year/MonthHandlerTest.java
@@ -16,91 +16,89 @@ import org.xml.sax.helpers.XMLReaderFactory;
 import persistency.DummyHandler;
 
 public class MonthHandlerTest {
-  private static YearFactory yf;
-  private static Reader monthInput;
-  private static XMLReader reader;
-  private static DummyHandler testHandler;
-  private static Month month;
-  private static MonthHandler monthHandler;
-  // We won't bother with SearchControl in these tests
-  private static SearchControl sc = null;
+	private static YearFactory yf;
+	private static Reader monthInput;
+	private static XMLReader reader;
+	private static DummyHandler testHandler;
+	private static Month month;
+	private static MonthHandler monthHandler;
 
-  @BeforeClass
-  public static void setUpBeforeClass() throws Exception {
-    yf = new YearFactory();
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		yf = new YearFactory();
 
-    try {
-      reader =
-        XMLReaderFactory.createXMLReader("org.apache.xerces.parsers.SAXParser");
-    }
-    catch (final SAXException e) {
-      try {
-        reader = XMLReaderFactory.createXMLReader();
-      } catch (final SAXException e1) {
-        throw new NoClassDefFoundError("No SAX parser is available.");
-      }
-    }
+		try {
+			reader =
+				XMLReaderFactory.createXMLReader("org.apache.xerces.parsers.SAXParser");
+		}
+		catch (final SAXException e) {
+			try {
+				reader = XMLReaderFactory.createXMLReader();
+			} catch (final SAXException e1) {
+				throw new NoClassDefFoundError("No SAX parser is available.");
+			}
+		}
 
-    /* We can't really test the enclosing year ID as it is set
-     * before entering the handler we're testing. */
-    final int monthId = 1;
-    final int yearId = 1;
+		/* We can't really test the enclosing year ID as it is set
+		 * before entering the handler we're testing. */
+		final int monthId = 1;
+		final int yearId = 1;
 
-    testHandler = new DummyHandler(reader);
-    month = new Month(monthId, yearId);
+		testHandler = new DummyHandler(reader);
+		month = new Month(monthId, yearId);
 
-    reader.setContentHandler(testHandler);
-    monthHandler = new MonthHandler(reader, testHandler, month, sc);
-  }
+		reader.setContentHandler(testHandler);
+		monthHandler = new MonthHandler(reader, testHandler, month);
+	}
 
-  @AfterClass
-  public static void tearDownAfterClass() throws Exception {
-    monthInput.close();
-  }
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		monthInput.close();
+	}
 
-  @Test
-  public final void testMonthHandlerSimple() throws Exception {
-    final int year = 1;
-    final int nrOfMonths = 1;
-    final int nrOfDaysEachMonth = 1;
-    final int nrOfActsEachDay = 1;
-    final YearConfig yc = new YearConfig(year, nrOfMonths, nrOfDaysEachMonth,
-                                         nrOfActsEachDay, sc);
+	@Test
+	public final void testMonthHandlerSimple() throws Exception {
+		final int year = 1;
+		final int nrOfMonths = 1;
+		final int nrOfDaysEachMonth = 1;
+		final int nrOfActsEachDay = 1;
+		final YearConfig yc = new YearConfig(year, nrOfMonths, nrOfDaysEachMonth,
+																				 nrOfActsEachDay);
 
-    final short testMonthIdKey = 1;
-    final Month testMonthKey = yf.getMonth(testMonthIdKey, yc);
+		final short testMonthIdKey = 1;
+		final Month testMonthKey = yf.getMonth(testMonthIdKey, yc);
 
-    final StringBuilder sb = new StringBuilder();
-    final String testMonthString = yf.getXmlMonth(nrOfMonths, yc, sb);
-    monthInput = new StringReader(testMonthString);
+		final StringBuilder sb = new StringBuilder();
+		final String testMonthString = yf.getXmlMonth(nrOfMonths, yc, sb);
+		monthInput = new StringReader(testMonthString);
 
-    testHandler.setHandlerToTest(monthHandler);
-    reader.parse(new InputSource(monthInput));
+		testHandler.setHandlerToTest(monthHandler);
+		reader.parse(new InputSource(monthInput));
 
-    assertEquals("Generated test object and key not equal!",
-                 testMonthKey, month);
-  }
+		assertEquals("Generated test object and key not equal!",
+								 testMonthKey, month);
+	}
 
-  @Test
-  public final void testMonthHandlerHarder() throws Exception {
-  	final int year = 1;
-    final int nrOfMonths = 1;
-    final int nrOfDaysEachMonth = 6;
-    final int nrOfActsEachDay = 6;
-    final YearConfig yc = new YearConfig(year, nrOfMonths, nrOfDaysEachMonth,
-                                           nrOfActsEachDay, sc);
+	@Test
+	public final void testMonthHandlerHarder() throws Exception {
+		final int year = 1;
+		final int nrOfMonths = 1;
+		final int nrOfDaysEachMonth = 6;
+		final int nrOfActsEachDay = 6;
+		final YearConfig yc = new YearConfig(year, nrOfMonths, nrOfDaysEachMonth,
+																					 nrOfActsEachDay);
 
-    final short testMonthIdKey = 1;
-    final Month testMonthKey = yf.getMonth(testMonthIdKey, yc);
+		final short testMonthIdKey = 1;
+		final Month testMonthKey = yf.getMonth(testMonthIdKey, yc);
 
-    final StringBuilder sb = new StringBuilder();
-    final String testMonthString = yf.getXmlMonth(nrOfMonths, yc, sb);
-    monthInput = new StringReader(testMonthString);
+		final StringBuilder sb = new StringBuilder();
+		final String testMonthString = yf.getXmlMonth(nrOfMonths, yc, sb);
+		monthInput = new StringReader(testMonthString);
 
-    testHandler.setHandlerToTest(monthHandler);
-    reader.parse(new InputSource(monthInput));
+		testHandler.setHandlerToTest(monthHandler);
+		reader.parse(new InputSource(monthInput));
 
-    assertEquals("Generated test object and key not equal!",
-                 testMonthKey, month);
-  }
+		assertEquals("Generated test object and key not equal!",
+								 testMonthKey, month);
+	}
 }
