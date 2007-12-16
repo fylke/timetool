111d1e3dcbb1043ef8f0b3849e0d78167a08ea68
magnus.falk <magnus.falk@d527ebd3-c326-0410-8431-79df68f5f61f>
2007-12-16 23:37:19 +0000

    Added the getXmlReader method

--- TimeTool/src/main/java/persistency/XmlUtils.java
+++ TimeTool/src/main/java/persistency/XmlUtils.java
@@ -2,60 +2,81 @@ package persistency;
 
 import org.joda.time.LocalTime;
 import org.joda.time.ReadableDateTime;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
 
 public class XmlUtils {
-  private static final int indentSize = 2;
-  private static final String indentationToken = "  ";
+	private static final int indentSize = 2;
+	private static final String indentationToken = "  ";
 
-  public ReadableDateTime stringToTime(final String hhmm,
-                                       final ReadableDateTime date) {
-    final String[] timeTokens = hhmm.split(":");
-    return new LocalTime(Integer.parseInt(timeTokens[0]),
-                         Integer.parseInt(timeTokens[1])).toDateTime(date);
-  }
+	public ReadableDateTime stringToTime(final String hhmm,
+																			 final ReadableDateTime date) {
+		final String[] timeTokens = hhmm.split(":");
+		return new LocalTime(Integer.parseInt(timeTokens[0]),
+												 Integer.parseInt(timeTokens[1])).toDateTime(date);
+	}
 
-  public String xmlify(final String stringToXmlify) {
-    return stringToXmlify.replace("&", "&amp;").replace("<", "&lt;").replace("'", "&apos;")
-                         .replace(">", "&gt;").replace("\"", "&quot;");
-  }
+	public String xmlify(final String stringToXmlify) {
+		return stringToXmlify.replace("&", "&amp;").replace("<", "&lt;").replace("'", "&apos;")
+												 .replace(">", "&gt;").replace("\"", "&quot;");
+	}
 
-  public String indent(final int level) {
-    final StringBuilder indentation = new StringBuilder();
-    for (int i = level; i > 0; i--) {
-      indentation.append(indentationToken);
-    }
+	public String indent(final int level) {
+		final StringBuilder indentation = new StringBuilder();
+		for (int i = level; i > 0; i--) {
+			indentation.append(indentationToken);
+		}
 
-    return indentation.toString();
-  }
+		return indentation.toString();
+	}
 
-  public String incIndent(final String indent) {
-    return indent + indentationToken;
-  }
+	public String incIndent(final String indent) {
+		return indent + indentationToken;
+	}
 
-  public String decIndent(final String indent) {
-    return indent.substring(indentSize, indent.length());
-  }
+	public String decIndent(final String indent) {
+		return indent.substring(indentSize, indent.length());
+	}
 
-  public StringBuilder getHeader(final String type) {
-    final StringBuilder sb = new StringBuilder();
-    sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n");
-    sb.append("<!DOCTYPE " + type + " SYSTEM \"" + type + ".dtd\">\n");
-    sb.append("<" + type + ">\n");
+	public StringBuilder getHeader(final String type) {
+		final StringBuilder sb = new StringBuilder();
+		sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n");
+		sb.append("<!DOCTYPE " + type + " SYSTEM \"" + type + ".dtd\">\n");
+		sb.append("<" + type + ">\n");
 
-    return sb;
-  }
+		return sb;
+	}
 
-  public StringBuilder getHeader(final String type, final String... attrs) {
-    final StringBuilder sb = new StringBuilder();
-    sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n");
-    sb.append("<!DOCTYPE " + type + " SYSTEM \"" + type + ".dtd\">\n");
+	public StringBuilder getHeader(final String type, final String... attrs) {
+		final StringBuilder sb = new StringBuilder();
+		sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n");
+		sb.append("<!DOCTYPE " + type + " SYSTEM \"" + type + ".dtd\">\n");
 
-    sb.append("<" + type);
-    for (final String element : attrs) {
-      sb.append(" " + element);
-    }
-    sb.append(">\n");
+		sb.append("<" + type);
+		for (final String element : attrs) {
+			sb.append(" " + element);
+		}
+		sb.append(">\n");
 
-    return sb;
-  }
+		return sb;
+	}
+
+	public XMLReader getXmlReader() throws NoClassDefFoundError {
+		XMLReader xmlReader;
+		try {
+			xmlReader = XMLReaderFactory.createXMLReader("org.apache.xerces.parsers.SAXParser");
+		} catch (final SAXException e) {
+			try {
+				xmlReader = XMLReaderFactory.createXMLReader();
+			} catch (final SAXException e1) {
+				throw new NoClassDefFoundError("No SAX parser is available.");
+			}
+		}
+
+		final EntityResolver resolver = new JarResolver();
+		xmlReader.setEntityResolver(resolver);
+		return xmlReader;
+	}
 }
