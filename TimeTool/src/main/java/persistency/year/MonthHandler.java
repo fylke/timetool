b1577897d401dc1ed8d3bdfcf117a1705e0c5740
magnus.falk <magnus.falk@d527ebd3-c326-0410-8431-79df68f5f61f>
2007-12-16 01:25:05 +0000

    Got rid of SearchControl

--- TimeTool/src/main/java/persistency/year/MonthHandler.java
+++ TimeTool/src/main/java/persistency/year/MonthHandler.java
@@ -10,55 +10,49 @@ import org.xml.sax.helpers.DefaultHandler;
 
 
 public class MonthHandler extends DefaultHandler {
-  private final CharArrayWriter text;
-  private final XMLReader reader;
-  private final ContentHandler parentHandler;
-  private final Month currentMonth;
-  private final SearchControl wanted;
+	private final CharArrayWriter text;
+	private final XMLReader reader;
+	private final ContentHandler parentHandler;
+	private final Month currentMonth;
 
-  public MonthHandler(final XMLReader reader,
-                      final ContentHandler parentHandler,
-                      final Month currentMonth,
-                      final SearchControl wanted)
-      throws SAXException {
-    this.wanted = wanted;
-    this.currentMonth = currentMonth;
-    this.parentHandler = parentHandler;
-    this.reader = reader;
+	public MonthHandler(final XMLReader reader,
+											final ContentHandler parentHandler,
+											final Month currentMonth)
+			throws SAXException {
+		this.currentMonth = currentMonth;
+		this.parentHandler = parentHandler;
+		this.reader = reader;
 
-    text = new CharArrayWriter();
-  }
+		text = new CharArrayWriter();
+	}
 
-  @Override
-  public void startElement(final String uri, final String localName,
-                           final String qName, final Attributes attrs)
-      throws SAXException {
-    text.reset();
+	@Override
+	public void startElement(final String uri, final String localName,
+													 final String qName, final Attributes attrs)
+			throws SAXException {
+		text.reset();
 
-    if ("workDay".equals(qName)) {
-      assert(currentMonth != null);
-      final int year = currentMonth.getEnclosingYear();
-      final int month = currentMonth.getId();
-      final int dayInMonth = Integer.parseInt(attrs.getValue("date"));
-      final WorkDay workDay = new WorkDay(year, month, dayInMonth);
+		if ("workDay".equals(qName)) {
+			assert(currentMonth != null);
+			final int year = currentMonth.getEnclosingYear();
+			final int month = currentMonth.getId();
+			final int dayInMonth = Integer.parseInt(attrs.getValue("date"));
+			final WorkDay workDay = new WorkDay(year, month, dayInMonth);
 
-      if ((wanted == null) || wanted.isHit(workDay.getDate())) {
-        currentMonth.addWorkDay(workDay);
+			currentMonth.addWorkDay(workDay);
 
-        final ContentHandler workDayHandler =
-          new WorkDayHandler(reader, this, workDay);
+			final ContentHandler workDayHandler = new WorkDayHandler(reader, this, workDay);
 
-        reader.setContentHandler(workDayHandler);
-      }
-    }
-  }
+			reader.setContentHandler(workDayHandler);
+		}
+	}
 
-  @Override
-  public void endElement(final String uri, final String localName,
-                         final String qName)
-      throws SAXException {
-    if ("month".equals(qName)) {
-      reader.setContentHandler(parentHandler);
-    }
-  }
+	@Override
+	public void endElement(final String uri, final String localName,
+												 final String qName)
+			throws SAXException {
+		if ("month".equals(qName)) {
+			reader.setContentHandler(parentHandler);
+		}
+	}
 }
