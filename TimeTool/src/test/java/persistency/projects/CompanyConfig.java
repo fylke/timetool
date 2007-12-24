0ff1366a449042d99e1115cdd5513127a8449ff4
magnus.falk <magnus.falk@d527ebd3-c326-0410-8431-79df68f5f61f>
2007-12-24 01:16:23 +0000

    Working after refactoring

--- TimeTool/src/test/java/persistency/projects/CompanyConfig.java
+++ TimeTool/src/test/java/persistency/projects/CompanyConfig.java
@@ -3,17 +3,17 @@ package persistency.projects;
 public class CompanyConfig {
 	final int compId;
 	final int nrOfProjsPerComp;
-	final int nrOfActsPerProj;
+	final int nrOfActsPerComp;
 	final int projDepth;
 
 	public CompanyConfig(final int compId,
 											 final int nrOfProjsPerComp,
-											 final int nrOfActsPerProj,
+											 final int nrOfActsPerComp,
 											 final int projDepth) {
 		super();
 		this.compId = compId;
 		this.nrOfProjsPerComp = nrOfProjsPerComp;
-		this.nrOfActsPerProj = nrOfActsPerProj;
+		this.nrOfActsPerComp = nrOfActsPerComp;
 		this.projDepth = projDepth;
 	}
 }
